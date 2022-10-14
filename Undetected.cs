412dc521e2941590c34cb70385337e9b6226810f
Silverdev <82159533+silverff@users.noreply.github.com>
2022-10-14 03:17:06 -0300

    Create Undetected.cs

---
+++ Undetected.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autoclicker   
+{
+    [Obfuscation(Feature = "virtualization", Exclude = false)]
+    public static class Undetected
+    {
+        [Obfuscation(Feature = "virtualization", Exclude = false)]
+        public static string RandomString()
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numbers = "123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            for (int i = 2; i < 6; i++)
+            {
+                stringChars[i] = numbers[random.Next(numbers.Length)];
+            }
+            for (int i = 6; i < 8; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+    }
+}
